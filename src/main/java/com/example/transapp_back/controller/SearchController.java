51872136940c5e7e8a5f739a1a8c5aac9e1e0681
yuuki-train <yuuki323-sld51@kcf.biglobe.ne.jp>
2021-01-28 19:00:38 +0900

    transapp_back modified 0128

--- src/main/java/com/example/transapp_back/controller/SearchController.java
+++ src/main/java/com/example/transapp_back/controller/SearchController.java
@@ -3,8 +3,10 @@ package com.example.transapp_back.controller;
 import com.example.transapp_back.dao.SearchDAO;
 import com.example.transapp_back.entity.Trains;
 import com.example.transapp_back.logic.SearchLogic;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.bson.Document;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -12,10 +14,10 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@ResponseBody
 public class SearchController {
     @RequestMapping(value = "/search", method = {RequestMethod.GET})
-    public String search(@RequestParam Map<String,String> requestParams) throws Exception{
+    @ResponseBody
+    public String search(Model model, @RequestParam Map<String, String> requestParams) {
 
         boolean addFeeTrainCheck;
         try {
@@ -54,9 +56,17 @@ public class SearchController {
 
             ObjectMapper mapper = new ObjectMapper();
 
-            return mapper.writeValueAsString(sortList);
-        }catch(NullPointerException e){
+            String jsonData = mapper.writeValueAsString(sortList);
+
+            model.addAttribute("result", jsonData);
+
+            return jsonData;
+
+        }catch(NullPointerException eNull){
             return "出発駅と到着駅を正しく指定してください";
+        }catch(JsonProcessingException eJson){
+            eJson.printStackTrace();
+            return "JSON変換でエラーが発生しました。再検索ください。";
         }
 
     }
